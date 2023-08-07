a12bcaaa52ef996125dbd0741531fa2d559e4487
DennisDv24 <DennisDv24@proton.me>
2023-08-07 04:34:28 +0200

    evolution and uris

--- contracts/ImmutableEvolutionContract.sol
+++ contracts/ImmutableEvolutionContract.sol
@@ -3,6 +3,10 @@
 pragma solidity ^0.8.10;
 
 import "erc721a/contracts/ERC721A.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "./interfaces/ISharesHolder.sol";
+import "./interfaces/IEvolutionStrategy.sol";
+import "solady/src/utils/LibString.sol";
 
 /**
  * NONE: Staking disabled.
@@ -16,9 +20,16 @@ enum StakingType {
     NONE, CURRENT, ALIVE, CUMULATIVE
 }
 
+struct StakingConfig {
+    uint24 
+}
+
 /**
  * Note that all those timestamps are enough to calculate any
  * possible staking strategy (any `StakingType`).
+ * Note also that the actual total time staked will be:
+ *     `totalTimeStaked + (block.timestamp - lastTimeStaked)`
+ * Because this value is dynamic and depends on the chain state.
  */
 struct StakeTokenInfo {
     bool isStaked;
@@ -29,27 +40,52 @@ struct StakeTokenInfo {
 
 struct EvolutionConfig {
     StakingType evolutionStakeStrategy;
+    address evolutionStrategy;
 }
 
+struct RewardsConfig {
+    StakingType rewardsTakeStrategy;
+}
 
-contract EvolutionArchetype is ERC721A {
+struct GeneralConfig {
+    uint256 price;
+    string baseUri;
+}
 
-    //StakingConfig public stakingConfig;
+contract ImmutableEvolutionArchetype is ERC721A, Ownable {
+
     mapping (uint256 => StakeTokenInfo) private _tokenIdToStakeInfo;
+
+    EvolutionConfig private _evolutionConfig;
+    RewardsConfig private _rewardsConfig;
+    GeneralConfig private _config;
+    bool private _initialized;
 
     constructor(
         string memory name,
         string memory ticker
     ) ERC721A(name, ticker) {}
+    
+    function initialize(
+        EvolutionConfig memory evolutionConfig,
+        RewardsConfig memory rewardsConfig,
+        GeneralConfig memory config 
+    ) public onlyOwner {
+        _evolutionConfig = evolutionConfig;
+        _rewardsConfig = rewardsConfig;
+        _config = config;
+        _initialized = true;
+    }
 
-    function mint(uint256 quantity) external payable {
-        // TODO Add requires
+    function mint(uint16 quantity) external payable {
+        require(msg.value >= _config.price * quantity);
         _mint(msg.sender, quantity);
     }
 
     function stake(uint256 tokenId) public {
         require(ownerOf(tokenId) == msg.sender);
         StakeTokenInfo memory stake = _tokenIdToStakeInfo[tokenId];
+        require(!stake.isStaked);
         uint32 currentTime = uint32(block.timestamp);
 
         if (stake.firstTimeStaked == 0) stake.firstTimeStaked = currentTime;
@@ -62,8 +98,8 @@ contract EvolutionArchetype is ERC721A {
     function unstake(uint256 tokenId) public {
         require(ownerOf(tokenId) == msg.sender);
         StakeTokenInfo memory stake = _tokenIdToStakeInfo[tokenId];
+        require(stake.isStaked);
 
-        require(stake.lastTimeStaked != 0);
         stake.totalTimeStaked += uint32(block.timestamp) - stake.lastTimeStaked;
 
         stake.isStaked = false;
@@ -79,8 +115,29 @@ contract EvolutionArchetype is ERC721A {
         if (strategy == StakingType.ALIVE) 
             return block.timestamp - stake.firstTimeStaked;
         if (strategy == StakingType.CUMULATIVE) 
-            return block.timestamp + stake.totalTimeStaked;
+            return (block.timestamp - stake.lastTimeStaked) + stake.totalTimeStaked;
         return 0;
+    }
+    
+    function getEvolution(uint256 tokenId) public view returns (uint256) {
+        uint256 stake = getStake(tokenId, _evolutionConfig.evolutionStakeStrategy);
+        return IEvolutionStrategy(_evolutionConfig.evolutionStrategy).getEvolution(stake);
+    }
+
+    function tokenURI(uint256 tokenId) 
+        public 
+        view
+        virtual
+        override
+        returns (string memory)
+    {
+        if (bytes(_config.baseUri).length == 0) return "";
+        return string(abi.encodePacked(
+            _config.baseUri,
+            LibString.toString(getEvolution(tokenId)),
+            "/",
+            LibString.toString(tokenId)
+        ));
     }
 
 }
